601af9e4176f5a0989b2f7d576340f16ef2d38d2
Stanislav Dashevskyi <stdashevsky@gmail.com>
2014-08-19 00:24:06 +0200

    fixed install.sh script

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# install Docker (version 0.9.0) -------
 apt-get update
 apt-get -q -y install curl
 sh -c "curl https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 apt-get update
 apt-get -q -y install lxc-docker-0.9.0
-
-# install PIP and Python bindings ---------
 apt-get -q -y install python-pip
 pip install selenium
 pip install docker-py
-
-# final stuff --------
 apt-get -q -y install cgroup-lite
-echo "===== Please reboot your PC to finish the setup ====="
-#reboot
+reboot
+
