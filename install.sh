5d786d33e4bfb61a08169164c1a0b30cc4ffe3df
Stanislav Dashevskyi <standash@users.noreply.github.com>
2014-08-20 18:19:17 +0200

    added comments

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 apt-get update
+
+# install curl
 apt-get -q -y install curl
+
+# install docker 0.9.0
 sh -c "curl https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 apt-get update
 apt-get -q -y install lxc-docker-0.9.0
+
+# install Python package manager
 apt-get -q -y install python-pip
+
+# install Docker and Selenium Python bindings
 pip install selenium
 pip install docker-py
 apt-get -q -y install cgroup-lite
+
+# reboot the PC
 reboot
 
