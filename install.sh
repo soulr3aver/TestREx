cfbe8a97c5efde064f99e9e045a31a00c818c3eb
Stanislav Dashevskyi <stdashevsky@gmail.com>
2014-09-30 16:34:23 +0200

    Uprade TestREx to use latest docker versions

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ apt-get -q -y install curl
 sh -c "curl https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 apt-get update
-apt-get -q -y install lxc-docker-0.9.0
+apt-get -q -y install lxc-docker
 
 # install Python package manager
 apt-get -q -y install python-pip
